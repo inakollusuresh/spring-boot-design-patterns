d44dbe2ce9780f9ed0f63f9af86819253df36094
suresh-inakollu <suresh.inakollu@maersk.com>
2023-12-24 13:50:30 +0530

    Initial commit

---
+++ src/main/java/com/practice/springbootdesignpatterns/SpringBootDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springbootdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDesignPatternsApplication.class, args);
+	}
+
+}
